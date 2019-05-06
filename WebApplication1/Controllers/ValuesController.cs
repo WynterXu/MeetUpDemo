d4fb869b8201f83d9acf62a543fde2b230d51cc6
WynterXu <xuwynter@gmail.com>
2019-05-06 18:03:21 +0800

    save

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace WebApplication1.Controllers
 {
@@ -42,13 +43,27 @@ namespace WebApplication1.Controllers
         {
             using (HttpClient httpClient= new HttpClient())
             {
-                var postUrl = "https://secure.meetup.com/oauth2/access";
-                //httpClient.BaseAddress = new System.Uri("https://secure.meetup.com/oauth2/access");
+                var postUrl = new System.Uri("https://secure.meetup.com/oauth2/access");
                 httpClient.DefaultRequestHeaders.Add("ContentType", "application/x-www-form-urlencoded");
-                string param = $"client_id=uam3aph3b62gvn3qq3rlubmldr&client_secret=sv5r6e4jcitv3tc4mmpcm8087d&grant_type=authorization_code&redirect_uri=https://www.beyondsoft.com&code={code}";
-                var requestBody = new FormUrlEncodedContent()
-                httpClient.PostAsync(postUrl, requestBody);
-            } 
+                var paramList = new List<KeyValuePair<string, string>>();
+                paramList.Add(new KeyValuePair<string, string>("client_id", "uam3aph3b62gvn3qq3rlubmldr"));
+                paramList.Add(new KeyValuePair<string, string>("client_secret", "sv5r6e4jcitv3tc4mmpcm8087d"));
+                paramList.Add(new KeyValuePair<string, string>("grant_type", "authorization_code"));
+                paramList.Add(new KeyValuePair<string, string>("redirect_uri", "http://localhost:1294/Values/Code"));
+                paramList.Add(new KeyValuePair<string, string>("code", code));
+                var requestBody = new FormUrlEncodedContent(paramList);
+                var result = httpClient.PostAsync(postUrl, requestBody).Result.Content.ReadAsStringAsync().Result;
+                var token = JsonConvert.DeserializeObject<Dictionary<string,string>>(result)["access_token"];
+            }
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://api.meetup.com/2/open_events");
+                var param = "?topic=photo&time=,1d";
+                httpClient.DefaultRequestHeaders.Add("", "");
+            }
+
+
             return code;
         }
     }
